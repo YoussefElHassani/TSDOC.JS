facf7b1dd1826f689e895edbf405c644aa513caa
Youssef El Hassani <youssef.elhassani@gmail.com>
2020-11-18 14:15:02 +0100

    Analysis file updated

--- src/python/analyze_log.py
+++ src/python/analyze_log.py
@@ -3,6 +3,10 @@ import re
 import json
 import ast 
 
+from itertools import chain
+from collections import Counter
+from nltk.tokenize import wordpunct_tokenize
+
 path = './log/info.log'
 
 # Reading files
@@ -17,6 +21,8 @@ success_count = 0
 
 # initializing error type dictionary
 error_dict = {}
+# Initialiring error messages dictionary
+error_messages = {}
 
 # Printing error name
 for record in data:
@@ -26,12 +32,20 @@ for record in data:
         error_count += 1
         exception_dict = json.loads(exception)
         error_name = exception_dict["exception"]["name"]
+        error_message = exception_dict["exception"]["message"]
         if(error_name == "Error"):
             try:
                 error_name = exception_dict["exception"]["code"]
             except:
                 error_name = exception_dict["exception"]["name"]
+        # Adding error message to the error_messages dict
+        if error_name not in error_messages:
+            error_messages[error_name] = []
+            error_messages[error_name].append(error_message)
+        else:
+            error_messages[error_name].append(error_message)
 
+        # Counting the occurance of the error code
         if error_name not in error_dict:
             error_dict[error_name] = 1
         else:
@@ -45,3 +59,18 @@ with open('./log/log_analysis.json', 'w') as fp:
     
 print("Total number of errors: " + str(error_count))
 print("Total number of successes: " + str(success_count))
+
+# Finding most common words per error code 
+error_messages_tokens = {}
+for key in error_messages:
+    messages = error_messages[key]
+    array = (Counter(chain.from_iterable(wordpunct_tokenize(x) for x in messages)).most_common(10))
+    error_tokens = {}
+    for word, count in array:
+        error_tokens[word] = count
+    
+    error_messages_tokens[key] = error_tokens
+
+# Write error messages token dict to a file
+with open('./log/log_analysis_tokens.json', 'w') as fp:
+    json.dump(error_messages_tokens, fp, indent=4)
